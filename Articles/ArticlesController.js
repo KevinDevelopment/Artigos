73a8981fa6f155a65a1a23a6ec3f208af920d9b6
KevinDevelopment <kevincristhian@outlook.com>
2022-07-08 19:53:19 -0300

    :sparkles: feat: tela de categorias adicionada

--- Articles/ArticlesController.js
+++ Articles/ArticlesController.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Categories = require("../Categories/Categories");
+const Articles = require("./Articles");
+const slugify = require("slugify");
 
-router.get("/articles", (request, response) => {
+router.get("/admin/articles", (request, response) => {
     response.send("minha rota de articles")
 });
 
 router.get("/admin/articles/new", (request, response) => {
-    response.render("admin/articles/new")
+
+    Categories.findAll({raw: true}).then((Categories) => {
+
+        response.render("admin/articles/new", {
+            Categories: Categories
+        })
+    })
+
+    
 });
+
+router.post("/articles/save", (request, response) => {
+    const { title, body, categoria } = request.body;
+
+    Articles.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: categoria
+    }).then(() => {
+        response.redirect("/admin/articles")
+    });
+
+})
 
 module.exports = router;
